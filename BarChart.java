9f53e32e88d0663fafe5426fe00acfa673f34f4a
TinyBean24 <samaestrada3@gmail.com>
2022-10-01 08:07:01 -0700

    Add files via upload

---
+++ BarChart.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class BarChart {
+    public static void main(String[] args) {
+        int counter = 5;
+        int num1, num2, num3, num4, num5;
+
+        Scanner input1 = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+        Scanner input3 = new Scanner(System.in);
+        Scanner input4 = new Scanner(System.in);
+        Scanner input5 = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        System.out.print("Enter number: ");
+        System.out.print("Enter number: ");
+        System.out.print("Enter number: ");
+        System.out.print("Enter number: ");
+
+    }
+}
+
+/**
+ * Ex 5.16(Bar Chart Printing Program)
+ * 5.16 (Bar-Chart Printing Program) One interesting application of computers is to display
+ * graphs and bar charts. Write an application that reads five numbers between 1 and 30.
+ * For each number that’s read, your program should display the same number of adjacent asterisks.
+ * For example, if your program reads the number 7, it should display *******.
+ * Display the bars of asterisks after you read all five numbers.
+ */
